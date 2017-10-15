e16c68d4de970b1b41ab2527dfd3af5346972ca5
Alex <adminser@ncsu.edu>
2017-10-15 00:22:39 -0400

    Initial Commit

---
+++ binary_counter.ino
@@ -0,0 +1,14 @@
+//binary counter program
+//lights up leds in binary count sequence, from 0 to 7
+//Alexander Minser, 10/14/2017
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
